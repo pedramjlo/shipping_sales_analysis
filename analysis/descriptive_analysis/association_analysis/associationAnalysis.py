9148b8c330cf3c8687fc40eadff2385912f95e18
pedramjlo <pedram_sif@tuta.io>
2025-07-11 17:56:04 +0330

    added error catching in create mba dataset

--- analysis/descriptive_analysis/association_analysis/associationAnalysis.py
+++ analysis/descriptive_analysis/association_analysis/associationAnalysis.py
@@ -3,6 +3,15 @@ CREATING A TRANSACTION TABLE OUT OF THE CLEANED DATASET FOR THE MARKET BASKET AN
 """
 import pandas as pd
 from data_saving.dataSaver import DataSaver
+import logging
+
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
+
 
 
 
@@ -13,13 +22,19 @@ class CreateDataset:
 
 
     def create_mba_dataset(self):
-        # Group by invoice_id to get a list of products per transaction
-        transaction_data = self.df.groupby('order_id')['product_name'].apply(list).reset_index()
-        unique_products = self.df['product_name'].unique()
+        try:
+            # Group by invoice_id to get a list of products per transaction
+            transaction_data = self.df.groupby('order_id')['product_name'].apply(list).reset_index()
+            unique_products = self.df['product_name'].unique()
 
-        # Create a new DataFrame for one-hot encoding
-        one_hot_data = pd.DataFrame(0, index=transaction_data.index, columns=unique_products)
-        
+            # Create a new DataFrame for one-hot encoding
+            one_hot_data = pd.DataFrame(0, index=transaction_data.index, columns=unique_products)
+            
+            logging.info("Created a dataset for market basket analysis successfully")
+
+        except Exception as e:
+            logging.error(f"Failed to create a dataset for market basket analysis {e}")
+
 
         # SAVING THE NEW DATAFRAME
         saver = DataSaver()
